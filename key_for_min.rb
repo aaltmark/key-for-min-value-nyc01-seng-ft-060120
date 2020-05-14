b56d4001d9e9f0af94772baf203677bf403ace27
Adam Altmark <adamaltmark@gmail.com>
2020-05-14 16:18:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   min_value = 9999999
   min_key = ""
-  hash.each do |key, value|
+  name_hash.each do |key, value|
     if value < min_value
       min_value = value
       min_key = key
